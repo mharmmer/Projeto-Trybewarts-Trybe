62edb514e7b8b35f917a72605c84c36bf693a3bf
Matheus Harmmer <mharmmer@hotmai.com>
2022-09-16 21:15:20 -0300

    Criada nova branch

--- script.js
+++ script.js
@@ -21,80 +21,12 @@ const observations = document.getElementById('textarea');
 
 submitButton.disabled = true;
 
-function createProfile() {
-  const div = document.createElement('div');
-  div.setAttribute('id', 'form-data');
-  document.body.insertBefore(div, header);
-  const p1 = document.createElement('p');
-  p1.innerText = `Nome: ${nameValue.value} ${lastNameValue.value}`;
-  div.appendChild(p1);
-  const p2 = document.createElement('p');
-  p2.innerText = `Email: ${emailValue.value}`;
-  div.appendChild(p2);
-}
-
-function createSelectedHouse() {
-  const p3 = document.createElement('p');
-  p3.innerText = `Casa: ${houseValue.value}`;
-  document.getElementById('form-data').appendChild(p3);
-}
-
-function createSelectedFamily() {
-  let selectedFamily = '';
-  for (let index = 0; index < familyValues.length; index += 1) {
-    if (familyValues[index].checked) {
-      selectedFamily = familyValues[index].value;
-      break;
-    }
-  }
-  const p4 = document.createElement('p');
-  p4.innerText = `Família: ${selectedFamily}`;
-  document.getElementById('form-data').appendChild(p4);
-}
-
-function createSelectedSubjects() {
-  const subjects = [];
-  for (let index = 0; index < subjectValues.length; index += 1) {
-    if (subjectValues[index].checked) {
-      subjects.push(` ${subjectValues[index].value}`);
-    }
-  }
-  const p5 = document.createElement('p');
-  p5.innerHTML = `Matérias: ${subjects}`;
-  document.getElementById('form-data').appendChild(p5);
-}
-
-function createEvaluationRatio() {
-  let evaluation;
-  for (let index = 0; index < evaluationRatio.length; index += 1) {
-    if (evaluationRatio[index].checked) {
-      evaluation = evaluationRatio[index].value;
-      break;
-    }
-  }
-  const p6 = document.createElement('p');
-  p6.innerText = `Avaliação: ${evaluation}`;
-  document.getElementById('form-data').appendChild(p6);
-}
-
-function createObservation() {
-  const p7 = document.createElement('p');
-  p7.innerText = `Observações: ${observations.value}`;
-  document.getElementById('form-data').appendChild(p7);
-}
-
 function hideForms() {
   globalForm.style.visibility = 'hidden';
 }
 
 function generateInformation() {
   hideForms();
-  createProfile();
-  createSelectedHouse();
-  createSelectedFamily();
-  createSelectedSubjects();
-  createEvaluationRatio();
-  createObservation();
 }
 
 document.addEventListener('click', (e) => {
@@ -102,7 +34,6 @@ document.addEventListener('click', (e) => {
     validateLogin();
   }
   if (e.target.id === 'submit-btn') {
-    e.preventDefault();
     generateInformation();
   }
 });
