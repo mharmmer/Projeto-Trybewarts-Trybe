ebcddef7496f2d25d0486187dace567d2672ccf4
Matheus Harmmer <mharmmer@hotmai.com>
2022-09-16 21:21:38 -0300

    Deixado apenas o essencial no JS para iniciar

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-const inputEmail = document.getElementById('email');
-const inputPassword = document.getElementById('password');
 const checkedInfo = document.getElementById('agreement');
 const submitButton = document.getElementById('submit-btn');
 const globalForm = document.getElementById('evaluation-form');
-const header = document.getElementById('main');
-
-const nameValue = document.getElementById('input-name');
-const lastNameValue = document.getElementById('input-lastname');
-const emailValue = document.getElementById('input-email');
-
-const houseValue = document.getElementById('house');
-
-const familyValues = document.querySelectorAll('input[name=family]');
-
-const subjectValues = document.querySelectorAll('input[name=subject]');
-
-const evaluationRatio = document.querySelectorAll('input[name=rate]');
 
 const observations = document.getElementById('textarea');
 
