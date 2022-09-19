bca90c2505c764883231237fa8ff79c10d4ec23b
Bernardo Marques <bernardomp.dev@gmail.com>
2022-09-19 14:50:07 -0300

    contador corrigido no html, alteracao de algumas box-shadows

--- script.js
+++ script.js
@@ -47,47 +47,47 @@ const footer = document.getElementById('footer')
 const housesStyle = () => {
   if (houses.value === 'Gitnória') {
     header.style.backgroundColor = 'rgba(223 9 9 / 20%)';
-    header.style.boxShadow = 'rgba(223 9 9 / 20%) 0px 4px 3px -1px';
+    header.style.boxShadow = 'rgba(223 9 9 / 20%) 0px 4px 3px -1px, 0 0 40px rgb(8 7 16 / 60%)';
     headerH1.style.color = 'rgba(255 197 0 / 80%)';
 
     footer.style.backgroundColor = 'rgba(223 9 9 / 20%)';
-    footer.style.boxShadow = 'rgba(223 9 9 / 20%) 0px 4px 3px 6px';
+    footer.style.boxShadow = 'rgba(223 9 9 / 20%) 0px 4px 3px 6px, 0 0 40px rgb(8 7 16 / 60%)';
     footer.style.color = 'rgba(255 197 0 / 80%)';
 
   } else if (houses.value === 'Reactpuff') {
     header.style.backgroundColor = 'rgba(227 187 12 / 20%)';
-    header.style.boxShadow = 'rgba(227 187 12 / 20%) 0px 4px 3px -1px';
+    header.style.boxShadow = 'rgba(227 187 12 / 20%) 0px 4px 3px -1px, 0 0 40px rgb(8 7 16 / 60%)';
     headerH1.style.color = 'rgb(0 0 0 / 72%)';
 
     footer.style.backgroundColor = 'rgba(227 187 12 / 20%)';
-    footer.style.boxShadow = 'rgba(227 187 12 / 20%) 0px 4px 3px 6px';
+    footer.style.boxShadow = 'rgba(227 187 12 / 20%) 0px 4px 3px 6px, 0 0 40px rgb(8 7 16 / 60%)';
     footer.style.color = 'rgb(0 0 0 / 72%)';
 
   } else if (houses.value === 'Corvinode') {
     header.style.backgroundColor = 'rgba(0 10 144 / 20%)';
-    header.style.boxShadow = 'rgba(0 10 144 / 20%) 0px 4px 3px -1px';
+    header.style.boxShadow = 'rgba(0 10 144 / 20%) 0px 4px 3px -1px, 0 0 40px rgb(8 7 16 / 60%)';
     headerH1.style.color = 'rgb(255 225 0 / 80%)';
 
     footer.style.backgroundColor = 'rgba(0 10 144 / 20%)';
-    footer.style.boxShadow = 'rgba(0 10 144 / 20%) 0px 4px 3px 6px';
+    footer.style.boxShadow = 'rgba(0 10 144 / 20%) 0px 4px 3px 6px, 0 0 40px rgb(8 7 16 / 60%)';
     footer.style.color = 'rgb(255 225 0 / 80%)';
 
   } else if (houses.value === 'Pytherina') {
     header.style.backgroundColor = 'rgba(13 98 23 / 20%)';
-    header.style.boxShadow = 'rgba(13 98 23 / 20%) 0px 4px 3px -1px';
+    header.style.boxShadow = 'rgba(13 98 23 / 20%) 0px 4px 3px -1px, 0 0 40px rgb(8 7 16 / 60%)';
     headerH1.style.color = 'rgba(180 180 180 / 80%)';
 
     footer.style.backgroundColor = 'rgba(13 98 23 / 20%)';
-    footer.style.boxShadow = 'rgba(13 98 23 / 20%) 0px 4px 3px 6px';
+    footer.style.boxShadow = 'rgba(13 98 23 / 20%) 0px 4px 3px 6px, 0 0 40px rgb(8 7 16 / 60%)';
     footer.style.color = 'rgba(180 180 180 / 80%)';
 
   } else if (houses.value === 'noneOption') {
     header.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
-    header.style.boxShadow = 'rgb(255, 255, 255, 0.2) 0px 4px 3px -1px';
+    header.style.boxShadow = 'rgb(255, 255, 255, 0.2) 0px 4px 3px -1px, 0 0 40px rgb(8 7 16 / 60%)';
     headerH1.style.color = 'rgba(245, 245, 245, 0.900)';
 
     footer.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
-    footer.style.boxShadow = 'rgb(255, 255, 255, 0.2) 0px 4px 3px 6px';
+    footer.style.boxShadow = 'rgb(255, 255, 255, 0.2) 0px 4px 3px 6px, 0 0 40px rgb(8 7 16 / 60%)';
     footer.style.color = 'rgba(245, 245, 245, 0.900)';
   }
 }
@@ -109,10 +109,10 @@ textArea.addEventListener('input', updateText);
 
 const resetPage = () => {
   header.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
-  header.style.boxShadow = 'rgb(255, 255, 255, 0.2) 0px 4px 3px -1px';
+  header.style.boxShadow = 'rgb(255, 255, 255, 0.2) 0px 4px 3px -1px, 0 0 40px rgb(8 7 16 / 60%)';
   headerH1.style.color = 'rgba(245, 245, 245, 0.900)';
 
   footer.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
-  footer.style.boxShadow = 'rgb(255, 255, 255, 0.2) 0px 4px 3px 6px';
+  footer.style.boxShadow = 'rgb(255, 255, 255, 0.2) 0px 4px 3px 6px, 0 0 40px rgb(8 7 16 / 60%)';
   footer.style.color = 'rgba(245, 245, 245, 0.900)';
 }
